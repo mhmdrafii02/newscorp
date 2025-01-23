2860a6066208969276e9ada5bdd300f168c171d1
mhmdrafii02 <rafii021003@gmail.com>
2025-01-23 22:36:19 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\FrontController;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [FrontController::class, 'index'])->name('front.index');
+
+Route::get('/details/{article_news:slug}', [FrontController::class, 'details'])->name('front.details');
+
+Route::get('/category/{category:slug}', [FrontController::class, 'category'])->name('front.category');
+Route::get('/author/{author:slug}', [FrontController::class, 'author'])->name('front.author');
+Route::get('/search', [FrontController::class, 'search'])->name('front.search');
+
